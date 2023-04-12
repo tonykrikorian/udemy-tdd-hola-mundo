10444b369b61cd6767e0d82c3356f7bdd346f586
Tony Krikorian <tony.krikorian@outlook.com>
2023-04-11 22:36:03 -0400

    test: 💍 Caso exitoso de creación de posts

--- endpoints/posts/index.spec.js
+++ endpoints/posts/index.spec.js
@@ -1,5 +1,46 @@
+const postHandlers = require("./index");
+
 describe("Endpoints", () => {
   describe("posts", () => {
-    it.skip("Should create a posts", () => {});
+    it("Should create a posts", async () => {
+      const mockUsers = [
+        {
+          id: 1,
+        },
+        { id: 2 },
+      ];
+      const post = {
+        userId: 1,
+        id: 1,
+        body: "Cuerpo del post",
+        title: "Titulo del post",
+      };
+
+      const req = {
+        body: post,
+      };
+
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        send: jest.fn(),
+      };
+
+      const axios = {
+        get: jest.fn().mockResolvedValue({ data: mockUsers }),
+        post: jest.fn().mockResolvedValue({ data: { id: 1000 } }),
+      };
+
+      await postHandlers({ axios }).post(req, res);
+
+      expect(res.status.mock.calls).toEqual([[201]]);
+      expect(axios.post.mock.calls).toEqual([
+        ["https://jsonplaceholder.typicode.com/posts", post],
+      ]);
+
+      expect(axios.get.mock.calls).toEqual([
+        ["https://jsonplaceholder.typicode.com/users"],
+      ]);
+      expect(res.send.mock.calls).toEqual([[{ id: 1000 }]]);
+    });
   });
 });
